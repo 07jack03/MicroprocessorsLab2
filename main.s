61a18229190d2c2bf1cb99bfd79f598802f149fe
jacka <jacka@192.168.1.170>
2026-02-11 18:09:31 +0000

    Changed output to PORTC. MAde additional counter at 0x007

--- main.s
+++ main.s
@@ -9,10 +9,11 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
+	movwf	TRISC, A	    ; Port C all outputs
 	bra 	test
 loop:
-	movff 	0x06, PORTB
+	movff 	0x06, PORTC
+	movff	0x06, 0x07
 	incf 	0x06, W, A
 test:
 	movwf	0x06, A	    ; Test for end of loop condition
